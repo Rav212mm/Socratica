2c59ff6b76797d21f781954d0d004b52b870574a
Rav212mm <rafal.sabak@gmail.com>
2018-12-04 19:36:34 +0100

    Update 02_Python strongs.py
    End of lesson 2

--- 02_Python strongs.py
+++ 02_Python strongs.py
@@ -1,3 +1,12 @@
 message = "Meet me tonight."
-prrint(message)
+print(message)
+
+message2 = 'The clock strikes at midnight'
+print(message2)
+
+message3 = '''This is what I'm '''
+print(message3)
+
+message4 = 'This is what I\'m today'
+print(message4)
 
